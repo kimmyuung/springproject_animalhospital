f6baf74aebd6896416d760ab08cb215ff365c49a
kimmyuung <rlaaudgh1003@daum.net>
2022-07-01 16:16:43 +0900

    소셜 로그인 API 구현 완료

--- src/main/resources/static/js/board/mypet.js
+++ src/main/resources/static/js/board/mypet.js
@@ -12,3 +12,29 @@ function save(){
             }
         });
 }
+
+$(function() {
+    // Multiple images preview in browser
+    var imagesPreview = function(input, placeToInsertImagePreview) {
+
+        if (input.files) {
+            var filesAmount = input.files.length;
+               $(".preview").html("");
+            for (i = 0; i < 1; i++) {
+                var reader = new FileReader();
+
+                reader.onload = function(event) {
+                    $($.parseHTML('<img>')).attr('src', event.target.result).appendTo(placeToInsertImagePreview);
+                     $($.parseHTML('<img>')).attr('style', 'width:80%');
+                }
+
+                reader.readAsDataURL(input.files[i]);
+            }
+        }
+
+    };
+
+    $('#bimg').on('change', function() {
+        imagesPreview(this, 'div.preview');
+    });
+});
