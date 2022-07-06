3ae3f066d221eb91d6e4c50e11a461d4d48e9e20
kimmyuung <rlaaudgh1003@daum.net>
2022-07-06 11:25:35 +0900

    클러스터 완료

--- src/main/resources/static/js/map2.js
+++ src/main/resources/static/js/map2.js
@@ -106,22 +106,38 @@ $.ajax({
     console.log(result)
         list = result;
     }
-})
+});
 
 
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div
     mapOption = {
         center: new kakao.maps.LatLng(37.63457, 127.33838), // 지도의 중심좌표
-        level: 5, // 지도의 확대 레벨
+        level: 6, // 지도의 확대 레벨
         mapTypeId : kakao.maps.MapTypeId.ROADMAP // 지도종류
     };
 // 지도를 생성한다
 var map = new kakao.maps.Map(mapContainer, mapOption);
 
-let html ="";
-var markers = [];
+var clusterer = new kakao.maps.MarkerClusterer({
+            map: map,
+            averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
+            minLevel: 10, // 클러스터 할 최소 지도 레벨
+            styles: [{
+                width : '53px', height : '52px',
+                background: 'url(cluster.png) no-repeat',
+                color: '#000',
+                textAlign: 'center',
+                lineHeight: '54px'
+            }]
+    });
+
 kakao.maps.event.addListener(map, 'idle', function() {
-    html = "";
+
+
+let html ="";
+clusterer.clear(); // 클러스터 클리어
+
+
         // 지도 영역정보를 얻어옵니다
         var bounds = map.getBounds();
 
@@ -138,11 +154,15 @@ kakao.maps.event.addListener(map, 'idle', function() {
                     && neLatlng.La > list[i].logt
                     && neLatlng.Ma > list[i].lat
                 ) {
-                    // 지도에 마커를 생성하고 표시한다
-                    var marker = new kakao.maps.Marker({
-                        position: new kakao.maps.LatLng(list[i].lat, list[i].logt), // 마커의 좌표
-                        map: map // 마커를 표시할 지도 객체
-                    });
+
+                       let marker = new kakao.maps.Marker({
+                       position : new kakao.maps.LatLng(list[i].lat, list[i].logt),
+                       map : map
+                          });
+
+
+
+
                      // 마커에 클릭 이벤트를 등록한다 (우클릭 : rightclick)
                     kakao.maps.event.addListener(marker, 'click', function() {
                         alert(list[i].name);
@@ -152,9 +172,16 @@ kakao.maps.event.addListener(map, 'idle', function() {
                             '<div>'+list[i].name+'</div>'+
                             '<div>'+list[i].addr+'</div>'+
                         '</div>';
-                }//if end
+
+
+            clusterer.addMarker(marker);
+         } //if end
          }
+         console.log( clusterer );
          $("#sidebar").html( html );
+
+
+
 });
 
 function panTo(lat, logt) {
@@ -165,3 +192,4 @@ function panTo(lat, logt) {
     // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
     map.panTo(moveLatLon);
 }
+
