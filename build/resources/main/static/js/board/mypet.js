ef487bb1b994e9eae8cebb44f4ef74cb119f7707
jin7639 <jin-bobae@naver.com>
2022-07-08 16:38:21 +0900

    병원 검색 수정완료

--- build/resources/main/static/js/board/mypet.js
+++ build/resources/main/static/js/board/mypet.js
@@ -461,10 +461,8 @@ function reupdate(rno){
         });
 }
 function rereplyinput(rno){
-
         let html =
             '<input type="text" id="reply"">'+
             '<button type="button" onclick="rereply('+rno+')">답글</button>';
         $("#replyinput").html(html);
-
 }
